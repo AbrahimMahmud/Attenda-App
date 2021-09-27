f3ee9bc052bfc0071ef032cceb35733873a2d34c
AbrahimMahmud <abrahimmclutch21@gmail.com>
2021-09-27 09:15:27 -0400

    commit progress so far, log in and regisration phase complete

--- App.js
+++ App.js
@@ -1,21 +1,95 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './redux/reducers'
+import thunk from 'redux-thunk'
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
+import firebase from 'firebase';
+const firebaseConfig = {
+  apiKey: "AIzaSyC16hj_nJV6eRLYIVtdPgANAWQcCAwXh7o",
+  authDomain: "attendatest1.firebaseapp.com",
+  projectId: "attendatest1",
+  storageBucket: "attendatest1.appspot.com",
+  messagingSenderId: "536633913499",
+  appId: "1:536633913499:web:54ac5d35edd5ffa4a4163e",
+  measurementId: "G-MEGB0P7Z33"
+};
+
+if(firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig)
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import LandingScreen from './components/auth/Landing'
+import RegisterScreen from './components/auth/Register'
+import MainScreen from './components/Main'
+import LoginScreen from './components/auth/Login'
+
+const Stack = createStackNavigator();
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false
+    }
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if(!user) {
+        this.setState({
+          loggedIn: false,
+          loaded: true,
+        })
+      } else {
+        this.setState({
+          loggedIn: true,
+          loaded: true,
+        })
+      }
+    })
+  }
+
+  render() {
+    const { loggedIn, loaded } = this.state;
+    if(!loaded) {
+      return(
+        <View style = {{ flex: 1, justifyContent: 'center' }}>
+          <Text>
+            Loading...
+          </Text>
+        </View>
+      )
+    }
+
+    if(!loggedIn) {
+      return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName = "Landing">
+            <Stack.Screen name = "Landing" component = {LandingScreen} options = {{ headerShown: false }}/>
+            <Stack.Screen name = "Register" component = {RegisterScreen}/>
+            <Stack.Screen name = "Login" component = {LoginScreen}/>
+          </Stack.Navigator>
+        </NavigationContainer>
+      );
+    }
+    return(
+      <Provider store = {store}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName = "Main">
+            <Stack.Screen name = "Main" component = {MainScreen} options = {{ headerShown: false }}/>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
+    )
+  
+  }
+}
+
+export default App
---
+++ components/Main.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { fetchUser } from '../redux/actions/index'
+
+import ProfileScreen from './main/Profile'
+import MainScanScreen from './main/MainScan'
+
+const Tab = createBottomTabNavigator();
+
+export class Main extends Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+    render() {
+        return (
+            <Tab.Navigator>
+                <Tab.Screen name = "Scan QR" component = {MainScanScreen} 
+                options = {{ 
+                        headerShown: false,
+                        tabBarIcon: ({ color, size}) => (
+                            <MaterialCommunityIcons name = "qrcode-scan" color = {color} size = {26} />
+                        ),
+                    }}/>
+                <Tab.Screen name = "Profile" component = {ProfileScreen} 
+                options = {{ 
+                        headerShown: false,
+                        tabBarIcon: ({ color, size}) => (
+                            <MaterialCommunityIcons name = "account-circle" color = {color} size = {26} />
+                        ),
+                    }}/>
+            </Tab.Navigator>
+        )
+    }
+}
+
+const mapStateToProps = (store) => ({
+    currentUser: store.userState.currentUser
+})
+const mapDispatchProps = (dispatch) => bindActionCreators({fetchUser}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchProps)(Main);
